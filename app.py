ddbf3f6356c1c6dc5827c5aa7b66bd555e744c01
Mehak Garg <mgarg5_be23@thapar.edu>
2026-04-06 15:26:29 +0530

    add MySQL database connection and 24 seats

--- app.py
+++ app.py
@@ -1,10 +1,27 @@
 from flask import Flask
+from flask_mysqldb import MySQL
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
 
 app = Flask(__name__)
 
+app.config['MYSQL_HOST'] = os.getenv('MYSQL_HOST')
+app.config['MYSQL_USER'] = os.getenv('MYSQL_USER')
+app.config['MYSQL_PASSWORD'] = os.getenv('MYSQL_PASSWORD')
+app.config['MYSQL_DB'] = os.getenv('MYSQL_DB')
+app.secret_key = os.getenv('SECRET_KEY')
+
+mysql = MySQL(app)
+
 @app.route('/')
 def home():
-    return 'Library Seat System is working!'
+    cursor = mysql.connection.cursor()
+    cursor.execute("SELECT COUNT(*) FROM seats")
+    count = cursor.fetchone()
+    return f'Database connected! Total seats: {count[0]}'
 
 if __name__ == '__main__':
     app.run(debug=True)
+ 
